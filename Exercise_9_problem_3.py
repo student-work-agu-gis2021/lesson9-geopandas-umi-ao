22e66b02571cce355051b1e49063bcbc08be9b16
umiao <81938242+umi-ao@users.noreply.github.com>
2021-06-13 15:58:34 +0000

    finished problem 3

--- Exercise_9_problem_3.py
+++ Exercise_9_problem_3.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+ #!/usr/bin/env python
 # coding: utf-8
 
 # ## Problem 3: How long distance individuals have travelled? 
@@ -9,13 +9,13 @@
 # YOUR CODE HERE 1 to read data
 import geopandas as gpd
 from pyproj import CRS
-data=None
+data = gpd.read_file('Kruger_posts.shp')
 
 # - Check the crs of the input data. If this information is missing, set it as epsg:4326 (WGS84).
 # - Reproject the data from WGS84 to `EPSG:32735` -projection which stands for UTM Zone 35S (UTM zone for South Africa) to transform the data into metric system. (don't create a new variable, update the existing variable `data`!)"
 
 # YOUR CODE HERE 2 to set crs
-
+data = data.to_crs(epsg=32735)
 # CODE FOR TESTING YOUR SOLUTION
 
 # Check the data
@@ -30,7 +30,7 @@ print(data.crs)
 #  - Group the data by userid
 
 #  YOUR CODE HERE 3 to group 
-grouped=None
+grouped=data.groupby('userid')
 
 # CODE FOR TESTING YOUR SOLUTION
 
@@ -44,7 +44,19 @@ assert len(grouped.groups) == data["userid"].nunique(), "Number of groups should
 # YOUR CODE HERE 4 to set movements
 import pandas as pd
 from shapely.geometry import LineString, Point
-movements=None
+movements = gpd.GeoDataFrame(columns=['userid','geometry'])
+count = 0
+for key, group in grouped:
+    group = group.sort_values('timestamp')
+    if len(group['geometry'])>=2:
+        line = (LineString(list(group['geometry'])))
+    else:
+        line=None
+    count +=1
+    movements.at[count, 'userid'] = key
+    movements.at[count, 'geometry'] = line
+movements.crs = CRS.from_epsg(32735)
+movements.crs = CRS.from_epsg(32735)
 # CODE FOR TESTING YOUR SOLUTION
 
 #Check the result
@@ -58,7 +70,12 @@ print(movements["geometry"].head())
 # - Calculate the lenghts of the lines into a new column called ``distance`` in ``movements`` GeoDataFrame.
 
 # YOUR CODE HERE 5 to calculate distance
-
+def cal_distance(x):
+    if x['geometry'] is None:
+        return None
+    else:
+        return x['geometry'].length
+movements['distance'] = movements.apply(cal_distance, axis=1)
 # CODE FOR TESTING YOUR SOLUTION
 
 #Check the output
@@ -72,11 +89,15 @@ movements.head()
 #  - What was the maximum distance travelled in meters?
 
 # YOUR CODE HERE 6 to find max, min,mean of the distance.
-
+import statistics as st
+print(st.mean(movements['distance'].dropna())) 
+print(max(movements['distance'].dropna())) 
+print(min(movements['distance']))
 # - Finally, save the movements of into a Shapefile called ``some_movements.shp``
 
 # YOUR CODE HERE 7 to save as Shapefile
-
+fp = 'some_movements.shp'
+movements.to_file(fp)
 # CODE FOR TESTING YOUR SOLUTION
 
 import os
